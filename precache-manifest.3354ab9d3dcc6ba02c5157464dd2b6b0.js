c7426fc66b227fe66055fdf56078d54da4b4f6b2
Nisha <nishishendkar288@gmail.com>
2018-11-28 19:55:14 -0800

    Updates

---
+++ precache-manifest.3354ab9d3dcc6ba02c5157464dd2b6b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-house/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "b79fc0b86df2196e6199",
+    "url": "/burger-house/static/js/runtime~main.b79fc0b8.js"
+  },
+  {
+    "revision": "e3d936db2fce6e7e48d0",
+    "url": "/burger-house/static/js/main.e3d936db.chunk.js"
+  },
+  {
+    "revision": "1481822ff8ee2d48e65c",
+    "url": "/burger-house/static/js/1.1481822f.chunk.js"
+  },
+  {
+    "revision": "e3d936db2fce6e7e48d0",
+    "url": "/burger-house/static/css/main.22cdf1e7.chunk.css"
+  },
+  {
+    "revision": "8fbf55063f7fb03da7c89b2c87f6d8a6",
+    "url": "/burger-house/index.html"
+  }
+];
